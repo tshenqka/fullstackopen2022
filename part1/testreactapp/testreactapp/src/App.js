65251c206f31e8ce91ebd7cf1c286b79d3d33c5a
tshenqka <maximatron115@Gmail.com>
2022-07-05 16:38:30 -0400

    Completed props and state and hooks. Currently finished with app.js as at Part 1 c).

---
+++ part1/testreactapp/testreactapp/src/App.js
@@ -0,0 +1,59 @@
+import {useState} from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+const Button = ({ handleClick, text }) => {
+  return (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+  )
+
+}
+
+
+
+function App() {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='Left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+
+export default App
+
